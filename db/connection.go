9adbdfef36273a1ca4fe6571f1a2c4a1aa224e1e
caio pereira <caiomp87@gmail.com>
2021-05-30 21:27:18 -0300

    create database connection

---
+++ db/connection.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connect() (*mongo.Collection, context.Context, error) {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	mongodb := client.Database("nfl").Collection("teams")
+	return mongodb, context.Background(), nil
+}
